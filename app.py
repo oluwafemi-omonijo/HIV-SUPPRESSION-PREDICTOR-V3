dfe0d973c51a142f7f7631d0f11936134d512c5b
Your Name <youremail@example.com>
2026-01-11 14:17:04 +0100

    fix streamlit import

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title=="HIV PREDICTOR")
-st.title ("HIV SUPPREESION PREDICTOR")
-st.write("Streamlit is running, if you can see this, the issue is not streamlit")
+st.set_page_config(page_title="HIV PREDICTOR")
+st.title("HIV SUPPRESSION PREDICTOR")
+st.write("Streamlit is running ✅")
+
