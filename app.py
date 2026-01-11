b96d421a37543179f57d041bc8890d56cf0512bc
Your Name <youremail@example.com>
2026-01-11 14:02:55 +0100

    Debug: minimal Streamlit render

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title=="HIV PREDICTOR")
+ST.TITLE("HIV SUPPREESION PREDICTOR")
+st.write("Streamlit is running, if you can see this, the issue is not streamlit")
